4d6100a2694e33ae8fb9240e11a46a23cb2026a1
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2023-04-26 22:05:58 -0300

    lista de string

---
+++ listaString/atividade01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char nome[50];
+    int quantidade;
+
+    printf ("Digite seu nome: ");
+    fgets(nome, 50, stdin);
+
+    quantidade = strlen(nome) - 1;
+
+    printf("Seu texto tem %d caracteres", quantidade);
+}
