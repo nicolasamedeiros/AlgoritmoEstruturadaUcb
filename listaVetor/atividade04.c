f867152eae59eb7cc7b20659bc0609d6fe595752
Nicolas Almeida Medeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2023-05-02 21:20:09 -0300

    Final atividade 04 vetor

--- listaVetor/atividade04.c
+++ listaVetor/atividade04.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-    int vetor[80], i, nBusca, posicao[6], contador = 0;
+    int vetor[80], i = 0, nBusca, posicao[6], contador = 0;
+    char opcao[3];
 
-    for (i = 0; i < 80; i++)
+    do 
     {
         printf("Digite um valor: ");
         scanf("%d", &vetor[i]);
-    }
+        i++;
+        printf("Deseja incluir mais um numero ? Sim ou Nao");
+        gets(opcao);
+    }while (strcmp(opcao, "Sim") == 0 && i < 80);
+    
  
     printf("Digite o numero que deseja buscar: ");
     scanf("%d", &nBusca);
