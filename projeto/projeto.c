dc16c04d0cf4d5905c2a22a6a87fb556966686c1
Nicolas Almeida Medeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2023-05-08 14:32:25 -0300

    função preencherdados completa

--- projeto/projeto.c
+++ projeto/projeto.c
@@ -2,19 +2,51 @@
 #include<string.h>
 #include<stdlib.h>
 
-void preencherdados(int *id, int *vacina, char nome[][50], char email[][25], char sexo[][10], char endereco[][30], double *altura){
-    for (size_t i = 0; i < count; i++){
-        /* code */
+void preencherdados(int *vacina, char nome[][50], char email[][25], char sexo[][13], char endereco[][30], double *altura){
+    for (int i = 0; i < 1; i++){
+        printf("Digite o nome do usuario: ");
+        fgets(nome[i], 50, stdin);
+        do
+        {
+            printf("Digite a altura em metros: ");
+            scanf("%lf", &altura[i]);   
+        } while (altura[i] < 1 || altura[i] > 2);
+        fflush(stdin);
+        do
+        {
+            printf("Digite o email do usuario: ");
+            fgets(email[i], 25, stdin);
+            fflush(stdin);
+        } while (strchr(email[i], '@') == 0);
+        
+        do
+        {
+            printf("Digite o sexo do usuario: ");
+            fgets(sexo[i], 13, stdin);
+            fflush(stdin);
+        } while (strcmp(sexo[i], "masculino") == 0 && strcmp(sexo[i], "feminino") == 0 && strcmp(sexo[i], "indiferente") == 0);
+        printf("Digite o endereco do usuario: ");
+        fgets(endereco[i], 30, stdin);
+        printf("Usuario tomou vacina ? 1 - sim / 2 - nao: ");
+        scanf("%d", &vacina[0]);
+        getchar();
     }
     
 }
 
 int main()
 {
-    int id, vacina;
-    char nome[3][50], email[3][25], sexo[3][10], enderenco[3][30];
-    double altura;
+    int vacina, i;
+    char nome[3][50], email[3][25], sexo[3][13], enderenco[3][30];
+    double altura[3];
 
+    preencherdados(&vacina, nome, email, sexo, enderenco, altura);
+
+    for ( i = 0; i < 1; i++)
+    {
+        printf("Usuarios cadstrados\n");
+        printf(" Nome: %s Email: %s Sexo: %s Endereco: %s altura: %.2lf\n vacina: %d", nome[i], email[i], sexo[i], enderenco[i], altura[i], vacina);
+    }
     
     return 0;
 }
