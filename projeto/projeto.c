c6125ec98ff91643ddd1fc3cce17bd58f81c254b
nicolasamedeiros <61924838+nicolasamedeiros@users.noreply.github.com>
2023-05-13 00:07:45 -0300

    switch case e mostrar todos os dados

--- projeto/projeto.c
+++ projeto/projeto.c
@@ -3,15 +3,17 @@
 #include<stdlib.h>
 
 void preencherdados(int *vacina, char nome[][50], char email[][25], char sexo[][13], char endereco[][30], double *altura){
-    for (int i = 0; i < 1; i++){
+    
+    for (int i = 0; i < 2; i++){
         printf("Digite o nome do usuario: ");
         fgets(nome[i], 50, stdin);
+        fflush(stdin);
         do
         {
             printf("Digite a altura em metros: ");
             scanf("%lf", &altura[i]);   
+            fflush(stdin);
         } while (altura[i] < 1 || altura[i] > 2);
-        fflush(stdin);
         do
         {
             printf("Digite o email do usuario: ");
@@ -25,27 +27,41 @@ void preencherdados(int *vacina, char nome[][50], char email[][25], char sexo[][
             fgets(sexo[i], 13, stdin);
             fflush(stdin);
         } while (strcmp(sexo[i], "masculino") == 0 && strcmp(sexo[i], "feminino") == 0 && strcmp(sexo[i], "indiferente") == 0);
+        
         printf("Digite o endereco do usuario: ");
         fgets(endereco[i], 30, stdin);
         printf("Usuario tomou vacina ? 1 - sim / 2 - nao: ");
         scanf("%d", &vacina[0]);
         getchar();
     }
-    
 }
 
 int main()
 {
     int vacina, i;
-    char nome[3][50], email[3][25], sexo[3][13], enderenco[3][30];
+    char opcao, nome[3][50], email[3][25], sexo[3][13], enderenco[3][30];
     double altura[3];
 
-    preencherdados(&vacina, nome, email, sexo, enderenco, altura);
+    printf("Digite a opcao que deseja fazer: ");
+    scanf("%c", &opcao);
+    fflush(stdin);
 
-    for ( i = 0; i < 1; i++)
-    {
-        printf("Usuarios cadstrados\n");
-        printf(" Nome: %s Email: %s Sexo: %s Endereco: %s altura: %.2lf\n vacina: %d", nome[i], email[i], sexo[i], enderenco[i], altura[i], vacina);
+    switch (opcao){
+    case 'a': 
+        preencherdados(&vacina, nome, email, sexo, enderenco, altura);
+        break;
+
+    case 'b':
+
+    
+    case 'e':
+        for ( i = 0; i < 2; i++){
+            printf("Usuarios cadstrados\n");
+            printf(" Nome: %s Email: %s Sexo: %s Endereco: %s altura: %.2lf\n vacina: %d", nome[i], email[i], sexo[i], enderenco[i], altura[i], vacina);
+        }
+        break;
+    default:
+        break;
     }
     
     return 0;
